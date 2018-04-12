07c6fdf428976a277fa096f63470c32fc0fac661
Akihiko Odaki <nekomanma@pixiv.co.jp>
2018-04-13 02:04:25 +0900

    Bundle CSS file if CSS is not injected to DOM

--- src/parcelRequire.js
+++ src/parcelRequire.js
@@ -2,5 +2,7 @@ const path = require('path');
 const sourceLocation = parseInt(process.versions.node, 10) >= 8 ? 'parcel-bundler/src/' : 'parcel-bundler/lib/';
 
 module.exports = {
+  Asset: path.join(sourceLocation, 'Asset'),
+  CSSAsset: path.join(sourceLocation, 'assets/CSSAsset'),
   JSAsset: path.join(sourceLocation, 'assets/JSAsset')
 }
