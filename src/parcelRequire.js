1c35154a04d4df97a4493a557f63d65e8c753ba6
Jasper De Moor <jasperdemoor@gmail.com>
2018-05-03 10:29:55 +0200

    remove parcel require

--- src/parcelRequire.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const sourceLocation = parseInt(process.versions.node, 10) >= 8 ? 'parcel-bundler/src/' : 'parcel-bundler/lib/';
-
-module.exports = {
-  JSAsset: path.join(sourceLocation, 'assets/JSAsset')
-}
