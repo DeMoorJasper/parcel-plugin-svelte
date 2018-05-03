2c442982ef8461283e163c4d4c6d5b02e338e1dd
Jasper De Moor <jdemo@live.be>
2018-05-03 11:32:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 module.exports = function (bundler) {
-    // bundler.addAssetType('html', require.resolve('./SvelteAsset'));
     bundler.addAssetType('svelte', require.resolve('./SvelteAsset'));
 };
