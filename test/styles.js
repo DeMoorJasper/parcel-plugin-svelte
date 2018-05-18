6220c53e7d92fd1e37670ac1903789d9d5bcf703
Jasper De Moor <jasperdemoor@gmail.com>
2018-05-18 13:25:55 +0200

    fix test

--- test/styles.js
+++ test/styles.js
@@ -1,6 +1,8 @@
 const { setupBundler, run } = require('./utils');
 const assertBundleTree = require('parcel-assert-bundle-tree');
 const path = require('path');
+const fs = require('fs');
+const assert = require('assert');
 
 describe('styles', function() {
   it('Should create a basic svelte bundle with stylesheets', async function() {
@@ -11,5 +13,12 @@ describe('styles', function() {
       name: 'main.js',
       assets: ['main.js', 'component.svelte']
     });
+
+    let bundleContent = await new Promise(resolve => fs.readFile(bundle.name, (err, data) => {
+      if (err) return reject(err);
+      return resolve(data);
+    }));
+    
+    assert(bundleContent.toString().indexOf(', the time is') > 0, 'Should contain the component code');
   });
 });
