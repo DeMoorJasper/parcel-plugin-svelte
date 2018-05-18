aca87238b8b4cd39abfd7cb6d589c38f1e191e86
Jasper De Moor <jdemo@live.be>
2018-05-18 12:58:18 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,16 @@
 const path = require('path');
 
 function sanitize(input) {
-	return path
-		.basename(input)
-		.replace(path.extname(input), '')
-		.replace(/[^a-zA-Z_$0-9]+/g, '_')
-		.replace(/^_/, '')
-		.replace(/_$/, '')
-		.replace(/^(\d)/, '_$1');
+  return path.basename(input)
+	     .replace(path.extname(input), '')
+	     .replace(/[^a-zA-Z_$0-9]+/g, '_')
+	     .replace(/^_/, '')
+	     .replace(/_$/, '')
+	     .replace(/^(\d)/, '_$1');
 }
 
 function capitalize(str) {
-	return str[0].toUpperCase() + str.slice(1);
+  return str[0].toUpperCase() + str.slice(1);
 }
 
 exports.sanitize = sanitize;
