c5e83a459cfe9f2bb9dc97cb939b853f321893be
Jasper De Moor <jasperdemoor@gmail.com>
2019-04-26 16:24:48 +0200

    cleanup kinda

---
+++ svelte-3/svelte-3-example/src/App.svelte
@@ -0,0 +1,34 @@
+<p>
+  Hello { name }, the time is <span class="the-time">{ hours }:{ minutes }:{ seconds }</span>
+</p>
+
+<style>
+  .the-time {
+    font-weight: bold;
+  }
+</style>
+
+<script>
+	import { onMount } from 'svelte'
+	
+	export let name = 'Anonymous'
+	let time = new Date()
+	
+	onMount(() => {
+		const timer = setInterval(() => {
+			time = new Date()
+		}, 1000)
+		
+		return () => {
+			clearInterval(timer)
+		}
+	})
+	
+	let hours, minutes, seconds
+	
+	$: {
+		hours = time.getHours()
+		minutes = time.getMinutes()
+		seconds = time.getSeconds()
+	}
+</script>
