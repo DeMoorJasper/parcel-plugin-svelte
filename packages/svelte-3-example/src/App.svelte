9c57a7873c3666dc88e5e34e5bcbf065d32f587d
Jasper De Moor <jasperdemoor@gmail.com>
2020-05-10 10:04:25 +0200

    update example

--- packages/svelte-3-example/src/App.svelte
+++ packages/svelte-3-example/src/App.svelte
@@ -1,6 +1,28 @@
-<p>
-  Hello { name }, the time is <span class="the-time">{ hours }:{ minutes }:{ seconds }</span>
-</p>
+<script>
+  import { onMount } from 'svelte';
+
+  export let name = 'Anonymous';
+  let time = new Date();
+
+  onMount(() => {
+    const timer = setInterval(() => {
+      time = new Date();
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  });
+
+  let hours, minutes, seconds, image;
+
+  $: {
+    hours = time.getHours();
+    minutes = time.getMinutes();
+    seconds = time.getSeconds();
+    image = require('./blobaston.png');
+  }
+</script>
 
 <style>
   .the-time {
@@ -8,27 +30,13 @@
   }
 </style>
 
-<script>
-	import { onMount } from 'svelte'
-	
-	export let name = 'Anonymous'
-	let time = new Date()
-	
-	onMount(() => {
-		const timer = setInterval(() => {
-			time = new Date()
-		}, 1000)
-		
-		return () => {
-			clearInterval(timer)
-		}
-	})
-	
-	let hours, minutes, seconds
-	
-	$: {
-		hours = time.getHours()
-		minutes = time.getMinutes()
-		seconds = time.getSeconds()
-	}
-</script>
+<div>
+  <h1>Hello {name},</h1>
+  <p>
+    the time is
+    <span class="the-time">{hours}:{minutes}:{seconds}</span>
+  </p>
+  <div>
+    <img src={image} alt />
+  </div>
+</div>
